08ed05221ccaf5c7394f8e0b7bb22c0978467123
WALTER SILVESTRE COAN <walter.s@univille.br>
2023-06-05 21:42:07 -0300

    finalizacao da API

--- src/main/java/br/univille/projfabsoft2023api/api/ClienteControllerAPI.java
+++ src/main/java/br/univille/projfabsoft2023api/api/ClienteControllerAPI.java
@@ -5,8 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,4 +37,24 @@ public class ClienteControllerAPI {
         return new ResponseEntity<Cliente>(cliente,HttpStatus.OK);
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<Cliente> 
+                alterarCliente(@PathVariable("id") Cliente clienteAntigo, 
+                            @RequestBody Cliente clienteNovo){
+
+        clienteAntigo.setNome(clienteNovo.getNome());
+        clienteAntigo.setEndereco(clienteNovo.getEndereco());
+        clienteAntigo.setDataNascimento(clienteNovo.getDataNascimento());
+        clienteAntigo.setEmail(clienteNovo.getEmail());
+        clienteAntigo.setTelefone(clienteNovo.getTelefone());
+
+        service.save(clienteAntigo);
+        return new ResponseEntity<Cliente>(clienteAntigo,HttpStatus.OK);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Cliente> 
+                deleteCliente(@PathVariable("id") Cliente clienteAntigo){
+        service.delete(clienteAntigo.getId());
+        return new ResponseEntity<Cliente>(clienteAntigo,HttpStatus.OK);
+    }
 }
--- src/main/java/br/univille/projfabsoft2023api/repository/ClienteRepository.java
+++ src/main/java/br/univille/projfabsoft2023api/repository/ClienteRepository.java
@@ -1,6 +1,9 @@
 package br.univille.projfabsoft2023api.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.univille.projfabsoft2023api.entity.Cliente;
@@ -8,5 +11,5 @@ import br.univille.projfabsoft2023api.entity.Cliente;
 @Repository
 public interface ClienteRepository 
     extends JpaRepository<Cliente,Long>{
-    
+    List<Cliente> findByNomeContains(String nome);
 }
